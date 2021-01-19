d8f44621ffc7cb0f49fa2b5ef7d24f10f762d2a5
Mikhail <strwolfy@gmail.com>
2021-01-19 18:44:26 +0300

    Сделано!

--- src/main/java/Task/Main.java
+++ src/main/java/Task/Main.java
@@ -1,19 +1,19 @@
 package Task;
 
-import Task.model.User;
-import Task.service.UserServiceImpl;
+import Task.dao.UserDaoJDBCImpl;
 import Task.service.UserService;
-import Task.util.Util;
+import Task.service.UserServiceImpl;
 
 import java.sql.SQLException;
-import java.util.List;
 
 public class Main {
 
-
     public static void main(String[] args) throws SQLException {
 
+        //UserDaoJDBCImpl userService = new UserDaoJDBCImpl();
         UserService userService = new UserServiceImpl();
+        // создание таблицы
+        userService.createUsersTable();
 
         // добавление 4-х пользователей
         userService.saveUser("Alexey", "Trubov", (byte) 45);
@@ -22,7 +22,7 @@ public class Main {
         userService.saveUser("Andrey", "Shevtsov", (byte) 27);
 
         // вывод всех пользователей
-        List<User> userList =  userService.getAllUsers();
+        userService.getAllUsers();
 
         // очистка таблицы
         userService.cleanUsersTable();
